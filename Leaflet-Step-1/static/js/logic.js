52991e37e3e1f15c1a62dbdd1bcd24fb782cf091
hyh-sherry <49596048+hyh-sherry@users.noreply.github.com>
2019-10-05 23:51:13 -0400

    updates

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 var myMap = L.map("map", {
-  center: [37.09, -95.71],
-  zoom: 5
+  center: [40.09, -30.71],
+  zoom: 2
 });
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -20,31 +20,31 @@ d3.json(queryUrl, function(data) {
     var latitude = record.geometry.coordinates[1];
     var magnitude = record.properties.mag;
 
-    var color = "";
-    if (magnitude > 0 && magnitude <= 1) {
-      color = "LawnGreen";
-    }
-    else if (magnitude > 1 && magnitude <= 2) {
-      color = "YellowGreen";
-    }
-    else if (magnitude > 2 && magnitude <= 3) {
-      color = "Gold";
-    }
-    else if (magnitude > 3 && magnitude <= 4) {
-      color = "Orange";
-    }
-    else if (magnitude > 4 && magnitude <= 5) {
-      color = "LightSalmon";
-    }
-    else {
-      color = "LightCoral";
-    }
+    // var color = "";
+    // if (magnitude > 0 && magnitude <= 1) {
+    //   color = "LawnGreen";
+    // }
+    // else if (magnitude > 1 && magnitude <= 2) {
+    //   color = "YellowGreen";
+    // }
+    // else if (magnitude > 2 && magnitude <= 3) {
+    //   color = "Gold";
+    // }
+    // else if (magnitude > 3 && magnitude <= 4) {
+    //   color = "Orange";
+    // }
+    // else if (magnitude > 4 && magnitude <= 5) {
+    //   color = "LightSalmon";
+    // }
+    // else {
+    //   color = "LightCoral";
+    // }
 
     L.circle([latitude, longitude], {
       fillOpacity: 0.9,
       color: "black",
       weight: 0.5,
-      fillColor: color,
+      fillColor: getColor(magnitude),
       // Setting our circle's radius equal to the output of our markerSize function
       // This will make our marker's size proportionate to its population
       radius: magnitude*15000
@@ -55,23 +55,23 @@ d3.json(queryUrl, function(data) {
  });
 
 
-var legend = L.control({position: "bottomright"});
-
-
 function getColor(d) {
-  return d > 5 ? "LightCoral" :
-  d > 4? "LightSalmon" :
-  d > 3? "Orange" :
-  d > 2? "YellowGreen" :
-  "LawnGreen" ;
+  if (d > 0 && d <= 1) {return "LawnGreen"}
+  else if (d > 1 && d <= 2) {return "YellowGreen"}
+  else if (d > 2 && d <= 3) {return "Orange"}
+  else if (d > 3 && d <= 4) {return "LightSalmon"}
+  else {return "LightCoral"}
   };
+
+var legend = L.control({position: "bottomright"});
 
 legend.onAdd = function (map) {
   var div = L.DomUtil.create("div", "info legend");
   magnitude = ["1","2","3","4","5"];
   colors = ["LawnGreen","YellowGreen","Gold","Orange","LightSalmon","LightCoral"]
   for (var i = 0; i < magnitude.length; i++) {
-    div.innerHTML += "<i style='background:" + getColor(magnitude[i] + 1) + "'></i> " + magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+')};
+    div.innerHTML += "<i style='background:" + getColor(magnitude[i]) + "'></i> " + magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+  }
   return div;
   };
 legend.addTo(myMap);
